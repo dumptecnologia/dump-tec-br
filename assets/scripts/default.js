62078a9386c3c0f1202585811793bf06b22d178d
medeirosinacio <medeirosinacio@outlook.com>
2021-09-06 14:25:44 -0300

    comeco do main home

--- assets/scripts/default.js
+++ assets/scripts/default.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
+    //setTimeout(function () {
         document.getElementById('load-page-container').remove();
         AOS.init({
             duration: 400,
             easing: 'ease-in-out',
         });
-    }, 2000);
+   // }, 2000);
 });
