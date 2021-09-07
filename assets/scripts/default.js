a9948a22ed10c8cfe11904d06e09e268cf31ba45
medeirosinacio <medeirosinacio@outlook.com>
2021-09-07 12:37:01 -0300

    alinhado load icon page

--- assets/scripts/default.js
+++ assets/scripts/default.js
@@ -1,9 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
+function startPage() {
+    function showPage() {
         document.getElementById('load-page-container').remove();
         AOS.init({
             duration: 400,
             easing: 'ease-in-out',
         });
+    }
+
+    if (window.location.hostname === 'localhost') {
+        showPage();
+        return;
+    }
+
+    setTimeout(function () {
+        showPage();
     }, 2000);
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    startPage();
 });
